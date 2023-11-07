b3240b95d6475fca09d587e94b837e4bd4b2eee8
Anila1022 <145949979+Anila1022@users.noreply.github.com>
2023-11-06 20:03:56 -0500

    Add files via upload

---
+++ 2023-11-06-lab/test.js
@@ -0,0 +1,4 @@
+// JavaScript source code
+const urlRegex = /^(https?:\/\/)?(www\.)?[a-zA-Z0-9-]+(\.[a-zA-Z]{2,3}){1,2}$/;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const phoneRegex = /(\+\d{1,2}\s?)?(\(\d{3}\)|\d{3})[\s.-]?\d{3}[\s.-]?\d{4}/;
